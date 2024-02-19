ee70ac28254c5dd79c07241ae91d3a45280685d8
Aditya Wiguna <adityawiguna0@gmail.com>
2024-02-19 15:25:55 +0800

    init template and sample page

---
+++ src/components/ui/app-navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from "react-native";
+import { Flex } from "@ant-design/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+export const AppNavbar = () => {
+    return (
+      <NavigationContainer>
+        <Flex>
+
+        </Flex>
+      </NavigationContainer>
+    );
+}
